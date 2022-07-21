f34ba7b592a40ba3eb5468762135be769d1c74bc
Oberst472 <oberst47@gmail.com>
2022-07-21 16:03:31 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import PageMain from '@/pages/home/index.vue'
+</script>
+
+<template>
+    <el-container>
+      <el-header>Header</el-header>
+
+      <el-main>
+        <PageMain/>
+      </el-main>
+
+      <el-footer>Footer</el-footer>
+    </el-container>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>main</section>
+</template>
+
+
+<script lang="ts">
+export default {
+  name: 'PageHome'
+}
+</script>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+
+</style>
